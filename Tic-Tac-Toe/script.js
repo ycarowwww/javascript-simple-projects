d06796c8a33759938d968de830b4bf07c308b59b
Yzakiel1390 <yzakiel1390@gmail.com>
2024-06-23 21:25:12 -0300

    Refactoring in the Tic-Tac-Toe
    A relatively large refactoring of the JavaScript code.

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -4,55 +4,60 @@ document.addEventListener("DOMContentLoaded", () => {
     const divSlots = document.querySelectorAll('div[id^="slot"]');
     const slots = document.querySelectorAll('div[id^="slot"] > button');
     const slotsPlayers = document.querySelectorAll('div[id^="slot"] > button > img');
-    const winnerX = document.getElementById("winx");
-    const winnerO = document.getElementById("wino");
+    const slotX = document.getElementById("winx");
+    const slotO = document.getElementById("wino");
     const draw = document.getElementById("draw");
     let winnerXCount = document.querySelector("div#winx > p.punc-div-score");
     let winnerOCount = document.querySelector("div#wino > p.punc-div-score");
     let drawCount = document.querySelector("div#draw > p.punc-div-score");
     const restartButton = document.getElementById("restartbutton");
+    const playerChoosen = document.querySelectorAll('.first-player > div > button');
 
-    for (let i = 0; i < slots.length; i++) {
-        slots[i].addEventListener("click", () => {
-            if (slotsPlayers[i].style.scale == "" && !gameEnds) {
-                notifyPlayer(slotsPlayers[i]);
-                slotsPlayers[i].style.scale = ".7";
-
-                if (checkWin()) {
-                    if (actualPlayer) {
-                        winnerXCount.innerText++;
-                        winnerX.style.border = "2px solid #D4CBCB";
-                        winnerX.style.boxShadow = "0 0 5px rgba(255, 255, 255, 0.5)";
-                        winnerO.style.border = "0";
-                        winnerO.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.5)";
-                    } else {
-                        winnerOCount.innerText++;
-                        winnerO.style.border = "2px solid #D4CBCB";
-                        winnerO.style.boxShadow = "0 0 5px rgba(255, 255, 255, 0.5)";
-                        winnerX.style.border = "0";
-                        winnerX.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.5)";
-                    }
-                    return;
-                }
-                
-                if (!Array.from(slotsPlayers).some(img => img.style.scale == '')) {
-                    drawCount.innerHTML = parseInt(drawCount.innerHTML) + 1;
-                    winnerX.style.border = "0";
-                    winnerX.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.5)";
-                    winnerO.style.border = "0";
-                    winnerO.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.5)";
-                    draw.style.border = "2px solid #D4CBCB";
-                    draw.style.boxShadow = "0 0 5px rgba(255, 255, 255, 0.5)";
-                    gameEnds = true;
-                    return;
-                }
-                
-                actualPlayer = !actualPlayer;
-            }
-        });
+    for (let i = 0; i < playerChoosen.length; i++) {
+        playerChoosen[i].addEventListener("click", () => { startGame(playerChoosen[i].innerHTML) });
     }
 
-    restartButton.addEventListener("click", () => {
+    for (let i = 0; i < slots.length; i++) {
+        slots[i].addEventListener("click", () => { playerMove(i); });
+    }
+
+    restartButton.addEventListener("click", () => { restartGame() });
+
+    function startGame(player) {
+        actualPlayer = player == "X";
+        actualPlayer = !actualPlayer;
+        drawPlayer(slotsPlayers[0]);
+        slotsPlayers[0].src = "";
+        actualPlayer = !actualPlayer;
+        for (let i = 0; i < slots.length; i++) {
+            slots[i].removeAttribute("disabled");
+        }
+        document.getElementsByClassName("first-player")[0].classList.toggle("player-choosen");
+    }
+
+    function playerMove(slot) {
+        if (slotsPlayers[slot].style.scale != "" || gameEnds) {
+            return;
+        }
+        
+        drawPlayer(slotsPlayers[slot]);
+        slotsPlayers[slot].style.scale = ".7";
+
+        if (checkWin()) {
+            if (actualPlayer) {
+                addWins(winnerXCount, slotX, slotO);
+            } else {
+                addWins(winnerOCount, slotO, slotX);
+            }
+            return;
+        }
+        
+        if (!checkDraw()) {
+            actualPlayer = !actualPlayer;
+        }
+    }
+
+    function restartGame() {
         if (actualPlayer == null) {
             return;
         }
@@ -62,27 +67,38 @@ document.addEventListener("DOMContentLoaded", () => {
             slotsPlayers[i].src = "";
             divSlots[i].classList.remove("winnerslot");
             gameEnds = false;
-            notifyPlayer(slotsPlayers[0]); // Pass a random Slot (This will not affect the game).
+            drawPlayer(slotsPlayers[0]); // Pass a random Slot (This will not affect the game).
             slotsPlayers[0].src = "";
             actualPlayer = !actualPlayer;
         }
-    });
+    }
 
-    function notifyPlayer(slot) {
-        draw.style.border = "0";
-        draw.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.5)";
+    function highlightSlot(slot) {
+        slot.style.border = "2px solid #D4CBCB";
+        slot.style.boxShadow = "0 0 5px rgba(255, 255, 255, 0.5)";
+    }
+
+    function unHighlightSlot(slot) {
+        slot.style.border = "0";
+        slot.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.5)";
+    }
+
+    function addWins(winnerCount, slotWinner, slotLoser) {
+        winnerCount.innerText++;
+        highlightSlot(slotWinner);
+        unHighlightSlot(slotLoser);
+    }
+
+    function drawPlayer(slot) {
+        unHighlightSlot(draw);
         if (actualPlayer) {
             slot.src = "imgs/x-tictactoe.svg";
-            winnerO.style.border = "2px solid #D4CBCB";
-            winnerO.style.boxShadow = "0 0 5px rgba(255, 255, 255, 0.5)";
-            winnerX.style.border = "0";
-            winnerX.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.5)";
+            highlightSlot(slotO);
+            unHighlightSlot(slotX);
         } else {
             slot.src = "imgs/o-tictactoe.svg";
-            winnerX.style.border = "2px solid #D4CBCB";
-            winnerX.style.boxShadow = "0 0 5px rgba(255, 255, 255, 0.5)";
-            winnerO.style.border = "0";
-            winnerO.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.5)";
+            highlightSlot(slotX);
+            unHighlightSlot(slotO);
         }
     }
 
@@ -91,31 +107,23 @@ document.addEventListener("DOMContentLoaded", () => {
         
         for (let i = 0; i < 3; i++) {
             if (slotsPlayers[i * 3].src == slotsPlayers[i * 3 + 1].src && slotsPlayers[i * 3 + 1].src == slotsPlayers[i * 3 + 2].src && slotsPlayers[i * 3 + 2].style.scale != "") {
-                divSlots[i * 3].classList.add("winnerslot");
-                divSlots[i * 3 + 1].classList.add("winnerslot");
-                divSlots[i * 3 + 2].classList.add("winnerslot");
+                slotWinners(divSlots[i * 3], divSlots[i * 3 + 1], divSlots[i * 3 + 2]);
                 return true;
             }
 
             if (slotsPlayers[i].src == slotsPlayers[i + 3].src && slotsPlayers[i + 3].src == slotsPlayers[i + 6].src && slotsPlayers[i + 6].style.scale != "") {
-                divSlots[i].classList.add("winnerslot");
-                divSlots[i + 3].classList.add("winnerslot");
-                divSlots[i + 6].classList.add("winnerslot");
+                slotWinners(divSlots[i], divSlots[i + 3], divSlots[i + 6]);
                 return true;
             }
         }
 
         if (slotsPlayers[0].src == slotsPlayers[4].src && slotsPlayers[4].src == slotsPlayers[8].src && slotsPlayers[8].style.scale != "") {
-            divSlots[0].classList.add("winnerslot");
-            divSlots[4].classList.add("winnerslot");
-            divSlots[8].classList.add("winnerslot");
+            slotWinners(divSlots[0], divSlots[4], divSlots[8]);
             return true;
         }
 
         if (slotsPlayers[2].src == slotsPlayers[4].src && slotsPlayers[4].src == slotsPlayers[6].src && slotsPlayers[6].style.scale != "") {
-            divSlots[2].classList.add("winnerslot");
-            divSlots[4].classList.add("winnerslot");
-            divSlots[6].classList.add("winnerslot");
+            slotWinners(divSlots[2], divSlots[4], divSlots[6]);
             return true;
         }
 
@@ -123,20 +131,21 @@ document.addEventListener("DOMContentLoaded", () => {
         return false;
     }
 
-    const playerChoosen = document.querySelectorAll('.first-player > div > button');
-    for (let i = 0; i < playerChoosen.length; i++) {
-        playerChoosen[i].addEventListener("click", () => { startGame(playerChoosen[i].innerHTML) });
+    function slotWinners(...winnerSlots) {
+        for (let i = 0; i < winnerSlots.length; i++) {
+            winnerSlots[i].classList.add("winnerslot");
+        }
     }
 
-    function startGame(player) {
-        actualPlayer = player == "X";
-        actualPlayer = !actualPlayer;
-        notifyPlayer(slotsPlayers[0]);
-        slotsPlayers[0].src = "";
-        actualPlayer = !actualPlayer;
-        for (let i = 0; i < slots.length; i++) {
-            slots[i].removeAttribute("disabled");
+    function checkDraw() {
+        if (!Array.from(slotsPlayers).some(img => img.style.scale == '')) {
+            drawCount.innerHTML = parseInt(drawCount.innerHTML) + 1;
+            unHighlightSlot(slotX);
+            unHighlightSlot(slotO);
+            highlightSlot(draw);
+            gameEnds = true;
+            return true;
         }
-        document.getElementsByClassName("first-player")[0].classList.toggle("player-choosen");
+        return false;
     }
 });
