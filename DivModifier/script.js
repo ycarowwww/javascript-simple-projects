637f13a8ccf38b8031644f72e30f8f58d8ef1df9
Yzakiel1390 <yzakiel1390@gmail.com>
2024-06-28 22:28:22 -0300

    DivModifier Simple Project
    A DivModifier made in HTML, CSS and JavaScript. This project is similar to ColorFlipper, but it is related to a Div and changes more CSS properties

---
+++ DivModifier/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let div = document.getElementById("div");
+    let input = document.getElementById("divstyle");
+
+    input.addEventListener("input", () => {
+        div.style.backgroundColor = input.value;
+        div.style.borderRadius = input.value;
+        div.style.boxShadow = input.value;
+    });
+});
