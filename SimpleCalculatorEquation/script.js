58dc8254911b5abb9a65cd32fa91334b640853f4
Yzakiel1390 <yzakiel1390@gmail.com>
2024-07-01 18:21:04 -0300

    Improved Character Programming Logic
    Click Events for the Parentheses, Percentage, Operations and Dot Buttons have Improved Programming Logic.

--- SimpleCalculatorEquation/script.js
+++ SimpleCalculatorEquation/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const calculatorInput = document.getElementById("equationinput");
     const buttonClear = document.querySelector(".buttons--clear");
     const buttonParenthesis = document.querySelector(".buttons--parenthesis");
-    const buttonPercent = document.querySelector(".buttons--percent");
+    const buttonPercentage = document.querySelector(".buttons--percentage");
     const buttonsOperations = document.querySelectorAll(".buttons button.buttons--operations:not(.buttons--parenthesis, .buttons--percent)");
     const buttonPlusOrMinus = document.querySelector(".buttons--plusorminus");
     const buttonsNumbers = document.querySelectorAll(".buttons--numbers");
@@ -14,15 +14,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     buttonParenthesis.addEventListener("click", () => {
-        calculatorInput.value += "()";
+        if (["+", "-", "×", "÷", "("].includes(calculatorInput.value.slice(-1))) {
+            calculatorInput.value += "(";
+        } else if (calculatorInput.value.includes("(") && (calculatorInput.value.match(/\(/g) || []).length > (calculatorInput.value.match(/\)/g) || []).length) {
+            calculatorInput.value += ")";
+        } else if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "%", ")"].includes(calculatorInput.value.slice(-1))) {
+            calculatorInput.value += "×(";
+        } else {
+            calculatorInput.value += "(";
+        }
     });
 
-    buttonPercent.addEventListener("click", () => {
+    buttonPercentage.addEventListener("click", () => {
+        if (["+", "-", "×", "÷"].includes(calculatorInput.value.slice(-1))) {
+            calculatorInput.value = calculatorInput.value.slice(0, -1);
+        }
         calculatorInput.value += "%";
     });
 
     for (let i = 0; i < buttonsOperations.length; i++) {
         buttonsOperations[i].addEventListener("click", () => {
+            if (["+", "-", "×", "÷"].includes(calculatorInput.value.slice(-1))) {
+                calculatorInput.value = calculatorInput.value.slice(0, -1);
+            }
             calculatorInput.value += buttonsOperations[i].innerHTML;
         });
     }
@@ -38,7 +52,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     buttonDot.addEventListener("click", () => {
-        if (calculatorInput.value.length > 0) {
+        if (calculatorInput.value.split(/\+|\-|\×|\÷/).at(-1).includes(".")) return;
+        
+        if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(calculatorInput.value.slice(-1))) {
             calculatorInput.value += ".";
         }
     });
