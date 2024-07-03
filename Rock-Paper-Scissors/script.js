19fb187a42b1cb0f93ae2155b55f64e1e4c13064
Yzakiel1390 <yzakiel1390@gmail.com>
2024-07-03 19:29:18 -0300

    Swapping emojis for SVG images
    Button icons will no longer be emojis, but SVG images. Additionally, a flag for what happened in the match (win, draw or defeat) was added. A small refactoring of the JS code also occurred.

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let playerChoice = document.querySelector(".playerchoice button");
     let aiChoice = document.querySelector(".aichoice button");
 
+    const divsScoreboard = document.querySelectorAll(".scoreboard > div");
     let playerCount = document.getElementById("playercount");
     let drawCount = document.getElementById("drawcount");
     let aiCount = document.getElementById("aicount");
@@ -14,9 +15,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let aiMove = null;
     const moves = ["rock", "paper", "scissors"];
     let movesImages = new Map();
-    movesImages.set("rock", "&#x1F48E;");
-    movesImages.set("paper", "&#x1F4DC;");
-    movesImages.set("scissors", "&#x2702;");
+    movesImages.set("rock", "images/rock.svg");
+    movesImages.set("paper", "images/paper.svg");
+    movesImages.set("scissors", "images/scissors.svg");
 
     playerRock.addEventListener("click", () => { playerMoves(playerRock) });
     playerPaper.addEventListener("click", () => { playerMoves(playerPaper) });
@@ -31,36 +32,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function aiMoves() {
         aiMove = moves[Math.floor(Math.random() * 3)];
-        aiChoice.innerHTML = movesImages.get(aiMove);
+        aiChoice.innerHTML = `<img src="${movesImages.get(aiMove)}">`;
         calculateWinner();
     }
 
     function calculateWinner() {
+        let count = null;
         if (playerMove == "rock") {
-            if (aiMove == "paper") {
-                aiCount.innerHTML++;
-            } else if (aiMove == "scissors") {
-                playerCount.innerHTML++;
-            } else {
-                drawCount.innerHTML++;
-            }
+            count = aiMove == "paper" ? aiCount : (aiMove == "scissors" ? playerCount : drawCount);
         } else if (playerMove == "paper") {
-            if (aiMove == "scissors") {
-                aiCount.innerHTML++;
-            } else if (aiMove == "rock") {
-                playerCount.innerHTML++;
-            } else {
-                drawCount.innerHTML++;
-            }
+            count = aiMove == "scissors" ? aiCount : (aiMove == "rock" ? playerCount : drawCount);
         } else {
-            if (aiMove == "rock") {
-                aiCount.innerHTML++;
-            } else if (aiMove == "paper") {
-                playerCount.innerHTML++;
-            } else {
-                drawCount.innerHTML++;
-            }
+            count = aiMove == "rock" ? aiCount : (aiMove == "paper" ? playerCount : drawCount);
         }
+        count.innerHTML++;
+        highlightCount(document.querySelector(`.scoreboard > div:has(span#${count.id}`));
+    }
+
+    function highlightCount(count) {
+        for (let i = 0; i < divsScoreboard.length; i++) {
+            divsScoreboard[i].classList.remove("highlight");
+        }
+        count.classList.add("highlight");
     }
 
     function restartGame() {
