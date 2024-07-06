f4f2c160cea11fb48fa80dd6b7a9f2e12d1b7f7a
Yzakiel1390 <yzakiel1390@gmail.com>
2024-07-05 21:28:25 -0300

    Bugs with "Error" Result and Parentheses Fixed
    Now, when a button is pressed and has an "Error" result on the screen, the Error Text will disappear automatically. Furthermore, when an Open Parenthesis is the last character, only some operations can be used immediately afterwards. And finally, if there are some parentheses that were not closed when the "=" button is pressed, they will be automatically closed.

--- SimpleCalculatorEquation/script.js
+++ SimpleCalculatorEquation/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const calculatorInput = document.getElementById("equationinput");
     const buttonClear = document.querySelector(".buttons--clear");
-    const buttonParenthesis = document.querySelector(".buttons--parenthesis");
+    const buttonParentheses = document.querySelector(".buttons--parentheses");
     const buttonPercentage = document.querySelector(".buttons--percentage");
-    const buttonsOperations = document.querySelectorAll(".buttons button.buttons--operations:not(.buttons--parenthesis, .buttons--percent)");
+    const buttonsOperations = document.querySelectorAll(".buttons button.buttons--operations:not(.buttons--parentheses, .buttons--percentage)");
     const buttonPlusOrMinus = document.querySelector(".buttons--plusorminus");
     const buttonsNumbers = document.querySelectorAll(".buttons--numbers");
     const buttonDot = document.querySelector(".buttons--dot");
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", () => {
         calculatorInput.value = "";
     });
 
-    buttonParenthesis.addEventListener("click", () => {
+    buttonParentheses.addEventListener("click", () => {
+        errorInCalculation();
         if (["+", "-", "×", "÷", "("].includes(calculatorInput.value.slice(-1))) {
             calculatorInput.value += "(";
         } else if ((calculatorInput.value.match(/\(/g) || []).length > (calculatorInput.value.match(/\)/g) || []).length) {
@@ -26,14 +27,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     buttonPercentage.addEventListener("click", () => {
-        if (["+", "-", "×", "÷"].includes(calculatorInput.value.slice(-1))) {
+        errorInCalculation();
+        if (["+", "-", "×", "÷"].includes(calculatorInput.value.slice(-1)) && (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "%", ")"].includes(calculatorInput.value.slice(-2, -1)))) {
             calculatorInput.value = calculatorInput.value.slice(0, -1);
+        }
+        if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "%", ")"].includes(calculatorInput.value.slice(-1))) {
+            calculatorInput.value += "%";
         }
     });
 
     for (let i = 0; i < buttonsOperations.length; i++) {
         buttonsOperations[i].addEventListener("click", () => {
-            if (["+", "-", "×", "÷"].includes(calculatorInput.value.slice(-1))) {
+            errorInCalculation();
+            if (calculatorInput.value.slice(-1) === "" || ((calculatorInput.value.slice(-1) === "(" || (calculatorInput.value.slice(-2, -1) === "(")) && !["+", "-"].includes(buttonsOperations[i].innerHTML))) return;
+            else if (["+", "-", "×", "÷"].includes(calculatorInput.value.slice(-1))) {
                 calculatorInput.value = calculatorInput.value.slice(0, -1);
             }
             calculatorInput.value += buttonsOperations[i].innerHTML;
@@ -41,6 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     buttonPlusOrMinus.addEventListener("click", () => {
+        errorInCalculation();
         const number = calculatorInput.value.split(/\+|\-|\×|\÷/).at(-1).replaceAll("(", "");
         const numberStartIndex = calculatorInput.value.length - number.length;
 
@@ -57,6 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (let i = 0; i < buttonsNumbers.length; i++) {
         buttonsNumbers[i].addEventListener("click", () => {
+            errorInCalculation();
             if ([")", "%"].includes(calculatorInput.value.slice(-1))) {
                 calculatorInput.value += "×";
             }
@@ -66,6 +75,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     buttonDot.addEventListener("click", () => {
+        errorInCalculation();
         if (calculatorInput.value.split(/\+|\-|\×|\÷/).at(-1).includes(".")) return;
         
         if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(calculatorInput.value.slice(-1))) {
@@ -74,10 +84,27 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     buttonEquals.addEventListener("click", () => {
+        completeParentheses();
         try {
             calculatorInput.value = eval(calculatorInput.value.replaceAll("%", "/100").replaceAll("÷", "/").replaceAll("×", "*"));
+            if (isNaN(calculatorInput.value)) throw "Error";
         } catch (error) {
             calculatorInput.value = "Error";
         }
     });
+
+    function completeParentheses() {
+        let amountOfOpenParentheses = (calculatorInput.value.match(/\(/g) || []).length;
+        let amountOfClosedParentheses = (calculatorInput.value.match(/\)/g) || []).length;
+        
+        if (amountOfOpenParentheses > amountOfClosedParentheses) {
+            calculatorInput.value += ")".repeat(amountOfOpenParentheses - amountOfClosedParentheses);
+        }
+    }
+
+    function errorInCalculation() {
+        if (calculatorInput.value === "Error") {
+            calculatorInput.value = "";
+        }
+    }
 });
