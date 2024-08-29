f05545c4d49df10a41df29f14784b83acf7007c6
Yzakiel1390 <yzakiel1390@gmail.com>
2024-08-29 19:12:41 -0300

    Improved game logic and image switching
    Logic for defining the winner and for displaying images of the choices improved. Some unnecessary HTML tags removed, and some default page properties in CSS changed.

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,56 +1,54 @@
 const playerRockButton = document.getElementById("rock");
 const playerPaperButton = document.getElementById("paper");
 const playerScissorsButton = document.getElementById("scissors");
-
-let playerChoice = document.querySelector(".playerchoice button");
-let aiChoice = document.querySelector(".aichoice button");
-
+const playerChoice = document.querySelector(".playerchoice");
+const aiChoice = document.querySelector(".aichoice button");
 const scoreboard = document.querySelectorAll(".scoreboard > div");
-let playerCount = document.getElementById("playercount");
-let drawCount = document.getElementById("drawcount");
-let aiCount = document.getElementById("aicount");
-
+const playerCount = document.getElementById("playercount");
+const drawCount = document.getElementById("drawcount");
+const aiCount = document.getElementById("aicount");
+const playerImage = document.querySelector(".playerchoice img");
+const aiImage = document.querySelector(".aichoice img");
 let playerMove = null;
 let aiMove = null;
-const moves = ["rock", "paper", "scissors"];
-let movesImages = new Map();
-movesImages.set("rock", "images/rock.svg");
-movesImages.set("paper", "images/paper.svg");
-movesImages.set("scissors", "images/scissors.svg");
 
-playerRockButton.addEventListener("click", () => { playerMoves(playerRockButton) });
-playerPaperButton.addEventListener("click", () => { playerMoves(playerPaperButton) });
-playerScissorsButton.addEventListener("click", () => { playerMoves(playerScissorsButton) });
+playerRockButton.addEventListener("click", () => { playerMoves("rock") });
+playerPaperButton.addEventListener("click", () => { playerMoves("paper") });
+playerScissorsButton.addEventListener("click", () => { playerMoves("scissors") });
 
-function playerMoves(playerButton) {
+function playerMoves(choice) {
     restartGame();
-    playerMove = playerButton.id;
-    playerChoice.innerHTML = playerButton.innerHTML;
+    playerMove = choice;
+    playerImage.src = `images/${playerMove}.svg`;
     aiMoves();
 }
 
 function aiMoves() {
+    const moves = ["rock", "paper", "scissors"];
     aiMove = moves[Math.floor(Math.random() * 3)];
-    aiChoice.innerHTML = `<img src="${movesImages.get(aiMove)}">`;
+    aiImage.src = `images/${aiMove}.svg`;
     calculateWinner();
 }
 
 function calculateWinner() {
     let count = null;
-    if (playerMove === "rock") {
-        count = aiMove === "paper" ? aiCount : (aiMove === "scissors" ? playerCount : drawCount);
+    if (playerMove === aiMove) {
+        count = drawCount;
+    } else if (playerMove === "rock") {
+        count = aiMove === "paper" ? aiCount : playerCount;
     } else if (playerMove === "paper") {
-        count = aiMove === "scissors" ? aiCount : (aiMove === "rock" ? playerCount : drawCount);
+        count = aiMove === "scissors" ? aiCount : playerCount;
     } else {
-        count = aiMove === "rock" ? aiCount : (aiMove === "paper" ? playerCount : drawCount);
+        count = aiMove === "rock" ? aiCount : playerCount;
     }
     count.innerHTML++;
-    winnerCount(document.querySelector(`.scoreboard > div:has(span#${count.id}`));
+    countSlot = document.querySelector(`.scoreboard > div:has(span#${count.id})`)
+    winnerCount(countSlot);
 }
 
 function winnerCount(count) {
-    for (let i = 0; i < scoreboard.length; i++) {
-        scoreboard[i].classList.remove("winner");
+    for (let score of scoreboard) {
+        score.classList.remove("winner");
     }
     count.classList.add("winner");
 }
